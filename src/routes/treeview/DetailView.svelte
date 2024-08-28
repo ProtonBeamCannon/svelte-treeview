8a6552816f94d80a779c4c0f7222f64f21411d86
Anon <Anon>
2024-08-28 10:34:55 +0200

    another commmit

---
+++ src/routes/treeview/DetailView.svelte
@@ -0,0 +1,15 @@
+<script>
+  // export let node;
+  import { buttonValue } from "./store";
+</script>
+
+<main>
+  <div>Hello {$buttonValue}</div>
+</main>
+
+<style>
+  div {
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+</style>
---
+++ src/routes/treeview/File.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name;
+  export let type;
+  import { buttonValue } from "./store";
+  $: extension = name.slice(name.lastIndexOf(".") + 1);
+  let show = false;
+  const handleDetailClick = () => {
+    console.log("handleDetailClick", name);
+    console.log("handleDetailClick", type);
+
+    buttonValue.set(name);
+  };
+</script>
+
+<button on:click={handleDetailClick}>
+  <span style="background-image: url({extension}.svg)">{name}</span>
+</button>
+
+<style>
+  span {
+    padding: 0 0 0 1.5em;
+    background: 0 0.1em no-repeat;
+    background-size: 1em 1em;
+  }
+</style>
